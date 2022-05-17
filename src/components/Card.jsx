31660b85ac3b45c52449aa8f1ccdfe005720d22a
joshraiborde <joshraiborde@gmail.com>
2022-05-17 17:53:04 -0400

    abstracted to be reuseable

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 
-function Card() {
-  return
-(
-<div className="card img">
-    <div className="top">
-      <h2 className="name">{props.name}</h2>
-      <img className="circle-img" src={props.img} alt={props.name} />
-    </div>
-    <div className="bottom">
-      <p className="info">{props.phone}</p>
-      <p className="info">{props.email}</p>
-    </div>
-  </div>
+function Card(props) {
+  return (
+    <div className="card img">
+        <div className="top">
+          <h2 className="name">{props.name}</h2>
+          <img className="circle-img"
+            src={props.img}
+            alt={props.name}
+          />
+        </div>
+        <div className="bottom">
+          <p className="info">{props.tel}</p>
+          <p className="info">{props.email}</p>
+        </div>
+      </div>
   );
 }
 
